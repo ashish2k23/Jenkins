ce1614879ace56280569586d7a166bcbfe8cd02c
Ashish <ashish.srivastava@espire.com>
2023-03-09 21:25:19 +0530

    update-6

--- ValidateDeploymentDetails.py
+++ ValidateDeploymentDetails.py
@@ -11,8 +11,17 @@ print(api_url);
 response = requests.get(api_url)
 camunda_response = response.json()
 print(camunda_response)
-print("***************after JSON response*************** ")
-for key in camunda_response:
+print(f"Application name is {camunda_response['applicationname']['value']}")
+print(camunda_response['reqstatus']['value'])
+if APPLICATION_NAME == camunda_response['applicationname']['value'] && camunda_response['reqstatus']['value'] == 'Approved':
+	print("deplyment request is validated")
+else:
+	print("deplyment request is not validated")
+	
+
+print("***************after JSON response END*************** ")
+#for key in camunda_response:
    #print(key, '->', camunda_response[key])
-   print(key, '->',camunda_response[key].values())
+   #print(key, '->',camunda_response[key].values())
+
 
