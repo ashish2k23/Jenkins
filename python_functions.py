88f488c457d911e7fc3009ff5627c1da5b2d61df
Ashish <ashish.srivastava@espire.com>
2023-03-09 16:58:42 +0530

    update-5

--- python_functions.py
+++ python_functions.py
@@ -1,8 +1,11 @@
+import os
+
+
 def print_name(name, age):
     return print(f"My name is {name} and age {age}")
-	
 
 
-print_name('Ashish', 40)
+name = os.getenv("input1")
+print_name(name, 40)
 print_name('Akansha', 36)
 
