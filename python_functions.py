10a70ab924100f2e73f6e7b55e271f6d83e4940c
Ashish <ashish.srivastava@espire.com>
2023-03-09 17:43:19 +0530

    update-5

--- python_functions.py
+++ python_functions.py
@@ -5,7 +5,7 @@ def print_name(name, age):
     return print(f"My name is {name} and age {age}")
 
 
-TaskID = os.environ('TASK_ID')
+TaskID = os.environ['TASK_ID']
 print(TaskID)
 print_name('Ashish', 40)
 print_name('Akansha', 36)
